f0e15e9061c1046f3ce03863f54158eec49d65e0
Hayweezyana <ayotundeadeboyeje@gmail.com>
2022-09-13 12:41:36 -0700

    printalphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,20 @@
 #include "main.h"
-
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
 	int n;
+	int co = 0;
+	do
+	{
 
 	for (n = 'a'; n <= 'z'; n++)
-	{
 		_putchar(n);
-	}
 	_putchar('\n');
+		co++;
+	}
+	while (co <= 10);
 }
