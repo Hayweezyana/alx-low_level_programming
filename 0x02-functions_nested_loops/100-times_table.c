28e96da9b0af359b92d23efc25c4f30b37f08237
Hayweezyana <ayotundeadeboyeje@gmail.com>
2022-09-13 16:05:23 -0700

    correction 2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n)
 		for (num = 0; num <= n; num++)
 		{
 			_putchar('0');
-			{
+			
 				for (mult = 1; mult <= n; mult++)
 				{
 					_putchar(',');
@@ -38,6 +38,6 @@ void print_times_table(int n)
 					_putchar((prod % 10) + '0');
 				}
 				_putchar('\n');
-			}
 		}
 	}
+}
