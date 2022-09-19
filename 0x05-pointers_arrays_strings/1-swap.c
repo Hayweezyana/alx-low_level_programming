198abbf0068b7fcd8afdf2641d9807ce07048fe1
Hayweezyana <ayotundeadeboyeje@gmail.com>
2022-09-19 14:25:45 -0700

    correction

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/*
- * swap_int - swaps the values of two integers
+/**
+ * swap_int - check the code
  * @a: integer to swap
  * @b: integer to swap
  */
 
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
 	int c = *a;
 	*a = *b;
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * rev_string - reverses a string.
  * @s: input string.
  * Return: no return.
