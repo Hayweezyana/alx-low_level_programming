d2ec3221ed99075274164d8400ac7b6a9cf19895
Hayweezyana <ayotundeadeboyeje@gmail.com>
2022-09-21 11:44:12 -0700

    correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,17 +7,21 @@
  *Return: returns new concatenated string
  */
 
-char *_strncat(char *dest, char *src, int n)
+int main(void)
 {
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *p;
 
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
-
+    printf("%s\n", s1);
+    printf("%s", s2);
+    p = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", p);
+    p = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", p);
+    return (0);
 }
