8ddc02a464a61ea7f0b552cc628a05b1ac47eff7
Hayweezyana <ayotundeadeboyeje@gmail.com>
2022-09-29 10:12:12 -0700

    commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _print_rev_recursion - prints a string in reverse
@@ -13,4 +14,6 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
+	else
+		_putchar('\n');
 }
